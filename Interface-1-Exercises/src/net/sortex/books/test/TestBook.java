19dba4dc9c98506b85fcdab4428aea5bf1146dc5
Alexander Karlsson Göransson <alexanderkarlssongoransson@hotmail.com>
2022-05-28 15:45:08 +0200

    Finished Q5 according to solution

--- Interface-1-Exercises/src/net/sortex/books/test/TestBook.java
+++ Interface-1-Exercises/src/net/sortex/books/test/TestBook.java
@@ -11,16 +11,8 @@ public class TestBook {
                 new Book("Harry Potter", "2000"),
                 new Book("Programming", "2019"),
                 new Book("Databases", "2020"),
-                new Book("Systems integration", "2022")
+                new Book("Systems integration", "2022"),
         };
-
-        System.out.print("Unsorted: ");
-        System.out.println(Arrays.toString(books));
-        Arrays.sort(books); // default sort
-        System.out.print("Default sorted (title): ");
-        System.out.println(Arrays.toString(books));
-        Arrays.sort(books, new BookYearComparator()); // using a comparator
-        System.out.print("Sorted on year: ");
-        System.out.println(Arrays.toString(books));
+        Arrays.sort(books);
     }
 }
