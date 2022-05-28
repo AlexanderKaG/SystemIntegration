18d84ecbbbdf8de6763ad8e756f510d2a201cf28
Alexander Karlsson Göransson <alexanderkarlssongoransson@hotmail.com>
2022-05-28 17:14:20 +0200

    Finished Q5

--- it-is-possible-to-extend-your-own-classes/com/superpower/filebrowser/main/TextMain.java
+++ it-is-possible-to-extend-your-own-classes/com/superpower/filebrowser/main/TextMain.java
@@ -10,13 +10,18 @@ import com.superpower.filebrowser.FBJavaFile;
 public class TextMain {
 
     public static void main(String[] args) {
-        
-        List<FBFile> files = FileList.list();
+
+        String dir = ".";
+        if (args.length != 0) {
+            dir = args[0];
+        }
+
+        List<FBFile> files = FileList.list(dir);
 
         for (FBFile f : files) {
             System.out.println(f.thumbnail() + " " + f.name());
         }
-        
+
     }
 
 }
